415e63123d115a8ab667695e29e750d2612c5385
dimalepel <dimalepel@mail.ru>
2021-12-18 17:56:45 +0300

    feat: add function body

--- src/index.js
+++ src/index.js
@@ -1,3 +1,70 @@
 module.exports = function toReadable (number) {
-  
+  let numbersUnitsList = [
+      'zero',
+      'one',
+      'two',
+      'three',
+      'four',
+      'five',
+      'six',
+      'seven',
+      'eight',
+      'nine',
+      'ten',
+      'eleven',
+      'twelve',
+      'thirteen',
+      'fourteen',
+      'fifteen',
+      'sixteen',
+      'seventeen',
+      'eighteen',
+      'nineteen',
+      'twenty'
+  ];
+  let numbersDozensList = [
+      'twenty',
+      'thirty',
+      'forty',
+      'fifty',
+      'sixty',
+      'seventy',
+      'eighty',
+      'ninety'
+  ];
+
+  let result = '';
+
+  if (number >= 0 && number <= 20) {
+      result += numbersUnitsList[number];
+  }
+
+  if (number > 20 && number < 100) {
+      result += numbersDozensList[Math.floor(number / 10) - 2];
+
+      let numberUnits = number % 10;
+
+      if (numberUnits > 0) {
+          result += ' ' + numbersUnitsList[numberUnits];
+      }
+  }
+
+  if (number >= 100 && number < 1000) {
+      result += numbersUnitsList[Math.floor(number / 100)] + ' hundred';
+
+      let numberDozens = number % 100;
+      let numberUnits = numberDozens % 10;
+
+      if (numberDozens > 20) {
+          result += ' ' + numbersDozensList[Math.floor(numberDozens / 10) - 2];
+
+          if (numberUnits !== 0) {
+              result += ' ' + numbersUnitsList[numberUnits];
+          }
+      } else if (numberDozens !== 0) {
+          result += ' ' + numbersUnitsList[numberDozens];
+      }
+  }
+
+  return result;
 }
